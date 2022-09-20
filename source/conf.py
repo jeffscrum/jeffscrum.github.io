556ef0c7bc13d0c2a3fcecdd62f208c02d5c61e3
Jeff Scrum <58530641+jeffscrum@users.noreply.github.com>
2022-09-20 16:27:33 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -62,7 +62,7 @@ html_static_path = ['_static']
 html_context = {
   'display_github': True,
   'github_user': 'jeffscrum',
-  'github_repo': 'ITDrafts',
+  'github_repo': 'itdrafts',
   'github_version': 'master/source',
 }
 
