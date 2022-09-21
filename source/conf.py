cc946766876a04b548f057833c184c8a7cacf828
Jeff Scrum <58530641+jeffscrum@users.noreply.github.com>
2022-09-21 13:43:01 +0300

    favicon

--- source/conf.py
+++ source/conf.py
@@ -54,20 +54,25 @@ sitemap_filename = "sitemap.xml"
 favicons = [
     {
         "rel": "icon",
+        "href": "favicon/favicon.ico",
+        "type": "image/x-icon",
+    },
+    {
+        "rel": "icon",
         "sizes": "16x16",
-        "href": "_static/favicon/favicon-16x16.png",
+        "href": "favicon/favicon-16x16.png",
         "type": "image/png",
     },
     {
         "rel": "icon",
         "sizes": "32x32",
-        "href": "_static/favicon/favicon-32x32.png",
+        "href": "favicon/favicon-32x32.png",
         "type": "image/png",
     },
     {
         "rel": "apple-touch-icon",
         "sizes": "180x180",
-        "href": "_static/favicon/apple-touch-icon.png",
+        "href": "favicon/apple-touch-icon.png",
         "type": "image/png",
     },
 ]
